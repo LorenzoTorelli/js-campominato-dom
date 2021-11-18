0a1187dedb021e930348f9ecadeb93b88870da46
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-11-18 17:06:46 +0100

    esercizo completo

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,6 +3,18 @@ let container = document.getElementById("container-grid");
 let play = document.getElementById('play');
 let bombe = [];
 
+// crea array con caselle con bombe
+function creaBombe(livello) {
+    let bombe = [];
+    while(bombe.length < 16){
+        let a = Math.floor(Math.random() * (livello - 1) + 1);
+        if (!bombe.includes(a)) {
+            bombe.push(a);
+        }
+    }
+    return bombe;
+}
+
 // svuota la griglia
 function svuotaGriglia() {
     container.innerHTML = "";
@@ -19,66 +31,62 @@ function addSquare(livello, i) {
 
 
 // creazione griglia 
-function Gioca(livello, bombe) {
+function Gioca(livello, bombe, result, punteggio) {
     for (let i = 0; i< livello; i++) {
         const divElem = addSquare(livello, i);
         container.appendChild(divElem);
-        changeColor(divElem,bombe, livello);
-        // if(changeColor(divElem,bombe, livello) == false) {
-        //     console.log(" gioco finito" );
-        // }
-        // else{
-        //     console.log(" il gioco continua");
+        changeColor(divElem, bombe, result, punteggio, livello);
 
-        // }
     }
+    return punteggio;
 }
 
-// crea array con caselle con bombe
-function creaBombe(livello) {
-    let bombe = [];
-    for (let i = 0; i<Math.sqrt(livello); i++) {
-        let a = Math.floor(Math.random() * (livello - 1) + 1);
-        bombe.push(a);
-    }
-    return bombe;
-}
+
 
 // cambia colore al click 
-function changeColor(divElem, bombe, livello) {
-    let game = true;
+function changeColor(divElem, bombe, result, punteggio, livello) {
+    let e = document.getElementsByClassName("click-true");
     divElem.addEventListener("click", function () {
         if (bombe.includes(parseInt(divElem.textContent))) {
             divElem.classList.add("click-false");
-            game = false;
+            result.classList.add("loser");
+            result.innerHTML = "HAI PERSO !! "
+            let a = parseInt(e.length);
+            punteggio.innerHTML = "Il tuo Punteggio è: " + a ;
+            giocoFinito(bombe);
             }
         else{
-            divElem.classList.add("click-true");
+            let e = document.getElementsByClassName("click-true");
+            if (livello-16 > e.length + 1) {
+                divElem.classList.add("click-true");
+            }
+            else {
+                result.classList.add("loser");
+                result.innerHTML = "HAI VINTO !! "   
+                let a = parseInt(e.length) + 1;
+                punteggio.innerHTML = "Il tuo Punteggio è: " + a ; 
+                giocoFinito(bombe);
+            }
         }
-        
-    if (game == false) {
-        giocoFinito(livello, bombe);
 
-    }
-    // else {
-    //     console.log("hai vinto")
-    // }
+        
+
     });
 }
 
-
-// // gioco finito 
- function giocoFinito(result, bombe) {
-    let result = document.getElementById("lose");
-    console.log(result);
-    result.classList.add("loser");
+//gioco vinto 
+function giocoVinto (livello, punteggio) {
+    while(livello-16 >= punteggio) {
+        punteggio += 1;
+    }
+    
+}
+// gioco finito 
+ function giocoFinito(bombe) {
     let elements = document.getElementsByClassName("square");
     for (let i = 0; i< elements.length; i++) {
-
         if (bombe.includes(parseInt(elements[i].textContent))) {
             elements[i].classList.add("click-false");
-            
-
         }
         else{
             elements[i].classList.add("click-true");
@@ -89,9 +97,10 @@ function changeColor(divElem, bombe, livello) {
 
 play.addEventListener("click", function() {
     let select = document.getElementById('livello');
+    let result = document.getElementById("lose");
+    let punteggio = document.getElementById("punt");
     let livello = select.options[select.selectedIndex].value;
-    let bombe = creaBombe(livello) 
-    console.log(bombe);
+    let bombe = creaBombe(livello);
     svuotaGriglia();
-    Gioca(livello, bombe);
-})
+    Gioca(livello, bombe, result, punteggio);
+});
